4560834922dfa189935da0c6b10c58571e46b897
Matthew Riley <mriley@86borders.com>
2017-08-12 23:44:32 -0400

    Initial Commit

---
+++ CrytpoTicker/CrytpoTicker.h
@@ -0,0 +1,19 @@
+//
+//  CrytpoTicker.h
+//  CrytpoTicker
+//
+//  Created by Matthew Riley on 8/12/17.
+//  Copyright © 2017 Matthew Riley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CrytpoTicker.
+FOUNDATION_EXPORT double CrytpoTickerVersionNumber;
+
+//! Project version string for CrytpoTicker.
+FOUNDATION_EXPORT const unsigned char CrytpoTickerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CrytpoTicker/PublicHeader.h>
+
+
